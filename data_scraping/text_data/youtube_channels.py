a2170f657d530b9a3b27106bb642b63c45cb5b4b
adam.kellich <adam.kellich@tipsport.cz>
2025-10-06 11:21:49 +0200

    data scrapers

---
+++ data_scraping/text_data/youtube_channels.py
@@ -0,0 +1,82 @@
+league_to_youtube_channel_mapping = {
+    "Worlds_24_playoffs": "https://www.youtube.com/watch?v=E9uM_rX5tU8&list=PLVgS_BIOY01x5-oeFIMWonhzncNtH5lt8",
+    "Worlds_24": "https://www.youtube.com/watch?v=QcdFZ0-yaDs&list=PLVgS_BIOY01wd2KlxZFhZYyNc2VUR88b2",
+    "Worlds_23": "https://www.youtube.com/watch?v=8kcGwFL-_EM&list=PLVgS_BIOY01wk1FXaL6QJzBEGJb1laBS6",
+    "Worlds_23_playoffs": "https://www.youtube.com/watch?v=qvjdBFSxzQQ&list=PLVgS_BIOY01zY9km730C-JHqLg7yCAvzJ",
+    "Worlds_22": "https://www.youtube.com/watch?v=vQRAc-CYf6c&list=PLVgS_BIOY01yoxnEPrGGUHOUVVmH8YR_h",
+    "Worlds_22_playoffs": "https://www.youtube.com/watch?v=AqQ_orDW6Qo&list=PLVgS_BIOY01xA8mGQi45lnJaCVrv1tzZe",
+    "Worlds_21_playoffs": "https://www.youtube.com/watch?v=E5_qlYqfDnQ&list=PLVgS_BIOY01zNuAyd-yPAOXEXtSGwuALp",
+    "Worlds_21": "https://www.youtube.com/watch?v=NVnwVPSzSIg&list=PLVgS_BIOY01wHRqPiAqRW4htcPdJsQv1i",
+    "Worlds_20_playoffs": "https://www.youtube.com/watch?v=fxQKa4edyzY&list=PLVgS_BIOY01zLt5ZbbDAGdHV0qr671JjS",
+    "Worlds_20": "https://www.youtube.com/watch?v=ey0c7PkI7oY&list=PLVgS_BIOY01wEnY9xfOIZQLKBFMgQWNtR",
+
+    "MSI_25": "https://www.youtube.com/watch?v=TQiYG-tKakY&list=PLPZ7h6L6LC7WI5tWcNb3KkaQn0J35P_lG",
+    "MSI_24": "https://www.youtube.com/watch?v=vb_BAals-0U&list=PLcvpEVobSi9ezaBn5PAf9CBMaxKKNkQaQ",
+    "MSI_23": "https://www.youtube.com/watch?v=6AJ19SOeeFc&list=PLcvpEVobSi9dzirXhw31QDRl4tic5Ryvf",
+    "MSI_22": "https://www.youtube.com/watch?v=v84UscoOYcs&list=PLcvpEVobSi9dn6UhP8lnJxbzZoxy5qIlE",
+    "MSI_21": "https://www.youtube.com/watch?v=lMDZvLpEf7c&list=PLcvpEVobSi9fbxZJa8kLVVQvSxA9trGsf",
+    # 2020 msi cancelled
+
+    # LCK Global
+    # complete playlist (playoffs included), doublematch ve videu format
+    "LCK_25": "https://www.youtube.com/playlist?list=PLFZGI6Zp1bXzc-uR75jo97hPlrAibo8nz",  # full
+    # asi struktura byla tak annoying, ze to dali do jednoho playlistu (LCK cup, 1-2, roadto msi, 3-5, playin, playoff)
+    "LCK_24_spring": "https://www.youtube.com/watch?v=5zBuPUxTzPI&list=PLFZGI6Zp1bXz_Qup3Bir3k3sPVCuf69gf",
+    "LCK_24_summer": "https://www.youtube.com/watch?v=aTwjlrnCprA&list=PLFZGI6Zp1bXyjxn7McrjsxTD2Md4QbqWP",
+    "LCK_23_spring": "https://www.youtube.com/watch?v=qEaMQylQq6Q&list=PLFZGI6Zp1bXx2iV07RM9ZsS3Ppkx1-JN6",
+    "LCK_22_spring": "https://www.youtube.com/watch?v=MYvIILz646c&list=PLFZGI6Zp1bXz8xOkqvVplO4tqsO5Mfvbd",
+    "LCK_22_summer": "https://www.youtube.com/watch?v=Af_d0qrsBtk&list=PLFZGI6Zp1bXwr8qTmW8-NkLRbo8sYMqTc",
+    # nahore doublematch ve videu format
+
+    "LCK_21_summer": "https://www.youtube.com/watch?v=m5IY7xwPJW8&list=PLFZGI6Zp1bXxFGcDUhAZAb6meYKJBBXw7",
+    #covid fked up
+    "LCK_21_spring": "https://www.youtube.com/watch?v=WtLM0NGBExk&list=PLFZGI6Zp1bXyGAmYM5yZCmOaUTu0nrlwh",
+    #covid fked up
+    "LCK_20_summer": "https://www.youtube.com/watch?v=lR1EJIFjpdM&list=PLcvpEVobSi9d3QEdLSLj-dyLJfYDGPsRw",
+    "LCK_20_spring": "https://www.youtube.com/watch?v=zRMNOUp_Hhk&list=PLcvpEVobSi9e1K18ybMBwdMPir_ugpreX",
+
+
+    "LPL_25_summer_playoffs": "https://www.youtube.com/watch?v=aw6MZyek5Dw&list=PLVgS_BIOY01zEKSHbRWxdfcEnV-mNOBiZ",
+    "LPL_25_summer": "https://www.youtube.com/watch?v=cPMx2qcmBxc&list=PLVgS_BIOY01yvwFTyro9d0jOC4cguQhSA",
+    "LPL_25_spring": "https://www.youtube.com/watch?v=N4IBsKotIiE&list=PLVgS_BIOY01xxKnrQOO71mEafttWaYGNu",
+    "LPL_25_winter": "https://www.youtube.com/watch?v=88g2EHVouPI&list=PLVgS_BIOY01wkHUwxkDln61slmuUFcj96",
+    "LPL_25_winter_playoffs": "https://www.youtube.com/watch?v=GOFIkWbQyqc&list=PLVgS_BIOY01xLMCeVfUSyi22HQXW-hBYt",
+    "LPL_24_summer_playoffs": "https://www.youtube.com/watch?v=ZVnKZUGa8c0&list=PLVgS_BIOY01z4hlVEzH67cLG3q4Vptb7U",
+    "LPL_24_summer": "https://www.youtube.com/watch?v=L1QUipFpnj8&list=PLVgS_BIOY01z8qt5IBT6rizjhk2tzKGBX",
+    "LPL_24_spring_playoffs": "https://www.youtube.com/watch?v=AY2HUb3XNVs&list=PLVgS_BIOY01x-awyw9LoUpVo0xfbi5yva",
+    "LPL_24_spring": "https://www.youtube.com/watch?v=G0VCZ-zVitw&list=PLVgS_BIOY01wibEdVCe6N_fwkUBPhxCmh",
+    "LPL_23_summer_playoffs": "https://www.youtube.com/watch?v=NpswvjwDCI4&list=PLVgS_BIOY01xy0qOlvFHZ5-uorWEADgNA",
+    "LPL_23_summer": "https://www.youtube.com/watch?v=lAtFp6A7t7w&list=PLVgS_BIOY01wUwi4eDJ6f-PPqW0YP28ws",
+    "LPL_23_spring_playoffs": "https://www.youtube.com/watch?v=XZGqFgttcxM&list=PLVgS_BIOY01wFmfr53fyaDj6PZTloSUpX",
+    "LPL_23_spring": "https://www.youtube.com/watch?v=bnmGNfs_vls&list=PLVgS_BIOY01wFF8Bka8zZZbCjLFQ_fIHA",
+    "LPL_22_summer_playoffs": "https://www.youtube.com/watch?v=pAQvw7c80Ps&list=PLVgS_BIOY01xj0Zx6eZ0FPQSpCucCkgiY",
+    "LPL_22_summer": "https://www.youtube.com/watch?v=Iw_w8NGKxo0&list=PLVgS_BIOY01zyV5VwteWptu-MTdr6R1E1",
+    "LPL_22_spring_playoffs": "https://www.youtube.com/watch?v=ZFXQDb39qfM&list=PLVgS_BIOY01zGKnbT_a29Jb-InVmgX0IT",
+    "LPL_22_spring": "https://www.youtube.com/watch?v=xxtFcAk26ic&list=PLVgS_BIOY01yIwpE0KLRTB6oGDje41geb",
+    "LPL_21_summer_playoffs": "https://www.youtube.com/watch?v=i0WLsjJk7gg&list=PLVgS_BIOY01zjvSiAwTUvzwgsGHLXIB7p",
+    "LPL_21_summer": "https://www.youtube.com/watch?v=CcCpDMjcRDU&list=PLVgS_BIOY01yBTQpbWQqG-wfMQNTL5LOg",
+    "LPL_21_spring_playoffs": "https://www.youtube.com/watch?v=bdjGZ9GA4ws&list=PLVgS_BIOY01wpPA41KDgyDLDhfXP-jhPx&pp=0gcJCaIEOCosWNin",
+    "LPL_21_spring": "https://www.youtube.com/watch?v=aDnaOATeyCc&list=PLVgS_BIOY01y06JWW6IAp60w9-oGIV7SO",
+    # 2020 chybi
+    "LPL_20_summer_playoffs": "https://www.youtube.com/watch?v=pNxKvA3_qmY&list=PLVgS_BIOY01xJhrU7MLw2dNY-fuCx-SAB",
+    "LPL_20_spring": "https://www.youtube.com/watch?v=gUbRaVoqtzA&list=PLVgS_BIOY01x2-V4Bpz6UbOBRPHas3xLi",
+
+
+
+
+    # complete playlist (has playoffs...)
+    "LEC_25_summer": "https://www.youtube.com/watch?v=VXA6bLJT55M&list=PLyQvIOxTaHdT8AgF6zj62_5DRWJTknubR",
+    "LEC_25_spring": "https://www.youtube.com/watch?v=5tBZGGliiJo&list=PLcvpEVobSi9dRtxkDJGnheKg1itmjYZsw",
+    "LEC_25_winter": "https://www.youtube.com/watch?v=x9yZqUldPR8&list=PLcvpEVobSi9c6D1yfjvDbRgH-ov1VOq-h",
+    "LEC_24_summer": "https://www.youtube.com/watch?v=_W3OMTnlYcU&list=PLcvpEVobSi9cBh0zBak3XyLKLJpMjb5Rf",
+    "LEC_24_spring": "https://www.youtube.com/watch?v=rjZzQwfdJK4&list=PLcvpEVobSi9fnWUa-k77RcdPoHIbVIQe3",
+    "LEC_24_winter": "https://www.youtube.com/watch?v=uURhvxM0yPE&list=PLcvpEVobSi9cEY1b4IrAsl8mQQLGzgf_t",
+
+    #LEC 23-21 bud tyden po tydnu full VODs, nebo playlisty v highlight formatu
+
+    "LEC_20_spring": "https://www.youtube.com/watch?v=uEzovIoqowE&list=PLyQvIOxTaHdRkfk77nKgwAo75Pe0ktwBG",
+    "LEC_20_summer": "https://www.youtube.com/watch?v=huJZcJPbDn0&list=PLyQvIOxTaHdTrzQubUNQRU5G2GM2FKGXL",
+    # complete playlist
+
+
+}
---
+++ data_scraping/text_data/youtube_transcript_downloader.py
@@ -0,0 +1,38 @@
+import os
+from yt_dlp import YoutubeDL
+from youtube_channels import league_to_youtube_channel_mapping
+
+# Target source (e.g. playlist, channel, or video)
+
+
+output_dir = 'downloads'
+
+# Loop over leagues
+for league_id, url in league_to_youtube_channel_mapping.items():
+    if url:
+        # Create a league-specific folder
+        league_folder = os.path.join(output_dir, league_id)
+        if os.path.exists(league_folder):
+            print(f"Skipping league {league_id} (already downloaded)")
+            continue
+        os.makedirs(league_folder, exist_ok=True)
+
+        # Configure yt-dlp with a league-specific outtmpl
+        ydl_opts = {
+            'writesubtitles': True,
+            'writeautomaticsub': True,
+            'subtitleslangs': ['en'],
+            'skip_download': True,
+            'writeinfojson': True,
+            'outtmpl': os.path.join(league_folder, '%(title)s.%(ext)s'),
+            'quiet': False,
+            'nocheckcertificate': True,
+            'subtitlesformat': 'srt',
+            'overwrites': False,
+            'ignoreerrors': True,
+        }
+
+
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+
